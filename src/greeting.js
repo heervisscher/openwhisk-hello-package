7dfe9b0ad67ff7d8a77f89480e806a2d6d562353
feike visser <heervisscher@gmail.com>
2017-05-23 09:45:16 +0200

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -7,5 +7,6 @@
 function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'i/o insights fv richard';
-    return {payload:  'Hello, ' + name + ' from ' + place + ' !'};
+    var challenge = params.challenge || 'no challenge';
+    return challenge;
 }
